3b6ccae72fd8174572dc31a575dc002b991db15d
Kodai Suemitsu <e185701@ie.u-ryukyu.ac.jp>
2018-11-08 20:31:35 +0900

    enemy attack problem

--- src/test/jp/ac/uryukyu/ie/e185701/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185701/EnemyTest.java
@@ -15,9 +15,12 @@ public class EnemyTest {
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
         for(int i=0; i<10; i++) {
-            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+            if(hero.hitPoint > 0){
+                hero.wounded(0);
+            }
+            else
+                enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.hitPoint);
     }
-
 }

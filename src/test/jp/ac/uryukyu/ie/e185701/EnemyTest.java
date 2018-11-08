00b7675f418c86d4bb9710ab762ac0185a96c06d
Kodai Suemitsu <e185701@ie.u-ryukyu.ac.jp>
2018-11-08 16:40:20 +0900

    report3

--- src/test/jp/ac/uryukyu/ie/e185701/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185701/EnemyTest.java
@@ -1,26 +1,23 @@
 package jp.ac.uryukyu.ie.e185701;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EnemyTest {
-
+public class EnemyTest {
+    /**
+     * 「死んでる状態では攻撃できない」ことを確認。
+     */
     @Test
-    void attack() {
+    public void attack() {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
-        for (int i = 0; i < 10; i++) {
-            if (hero.hitPoint > 0) {
-                hero.wounded(0);
-            } else {
-                enemy.attack(hero);//乱数で0ダメージとなることもあるため、複数回実行してみる。
-
-            }
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
         assertEquals(heroHP, hero.hitPoint);
     }
+
 }

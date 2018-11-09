524116e9cdde570373558529b4c9a91c64258115
Kodai Suemitsu <e185701@ie.u-ryukyu.ac.jp>
2018-11-09 13:45:08 +0900

    step3

--- src/main/jp/ac/uryukyu/ie/e185701/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e185701/Hero.java
@@ -8,7 +8,7 @@ package jp.ac.uryukyu.ie.e185701;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Hero {
+public class Hero extends LivingThing{
     String name;
     int hitPoint;
     int attack;
@@ -21,34 +21,40 @@ public class Hero {
      * @param attack ヒーローの攻撃力
      */
     public Hero (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        super(name,maximumHP,attack);
+//        this.name = name;
+//        hitPoint = maximumHP;
+//        this.attack = attack;
+//        dead = false;
+        //System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
     /**
      * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
      * @return boolean
      */
-    public boolean isDead(){
-        return  dead;
-    }
+//    public boolean isDead(){
+//        return  dead;
+//    }
+//
+//    public String getName(){
+//        return name;
+//    }
+//
+//    /**
+//     * Enemyへ攻撃するメソッド。
+//     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+//     * @param e 攻撃対象
+//     */
+//    public void attack(Enemy e){
+//        int damage = (int)(Math.random() * attack);
+//        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
+//        e.wounded(damage);
+//    }
 
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Enemyへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param e 攻撃対象
-     */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
+    @Override
+    public void wounded(int damage) {
+        super.wounded(damage);
     }
 
     /**
@@ -56,11 +62,12 @@ public class Hero {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
-    public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
-        }
-    }
+
+//    public void wounded(int damage){
+//        hitPoint -= damage;
+//        if( hitPoint < 0 ) {
+//            dead = true;
+//            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+//        }
+//    }
 }
--- src/test/jp/ac/uryukyu/ie/e185701/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e185701/EnemyTest.java
@@ -1,26 +1,25 @@
 package jp.ac.uryukyu.ie.e185701;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class EnemyTest {
-    /**
-     * 「死んでる状態では攻撃できない」ことを確認。
-     */
+class EnemyTest {
+
     @Test
-    public void attack() {
+    void attack() {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
         enemy.dead = true;
-        for(int i=0; i<10; i++) {
-            if(hero.hitPoint > 0){
+        for (int i = 0; i < 10; i++) {
+            if (hero.getHitPoint() > 0) {
                 hero.wounded(0);
+            } else {
+                enemy.attack(hero);//乱数で0ダメージとなることもあるため、複数回実行してみる。
             }
-            else
-                enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP,hero.getHitPoint());
     }
 }
